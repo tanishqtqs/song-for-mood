837dd8d514a8f96f60c4357ebf8028df2659ed82
tanishqtqs <55868345+tanishqtqs@users.noreply.github.com>
2025-03-17 20:46:36 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+# from app.models.emotion_detector import detect_emotion
+# from app.models.song_recommender import retrieve_song
+
+app = FastAPI()
+
+class UserInput(BaseModel):
+    text: str
+
+# @app.post("/recommend")
+# def recommend_song(user_input: UserInput):
+    # mood = detect_emotion(user_input.text)
+    # song = retrieve_song(mood)
+    # return {"mood": mood, "recommendation": song}
+
